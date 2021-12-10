e29408ed0d6863438f5c24f71f6b47f3e4783676
RosaBri <94406766+RosaBri@users.noreply.github.com>
2021-12-10 02:03:02 -0500

    Update netsize_all.sh
    comando usado

--- netsize_all.sh
+++ netsize_all.sh
@@ -1,2 +1,3 @@
 # Segunda parte del deber Nº 1  de  Bioinformática
 ## Escriba un guión que imprima el número de filas y columnas para cada red
+#### - A continuación los comandos utilizados para resolver la segunda parte del deber
